ed81f54d65b46e257071e7daf133421c946f640b
Artur Kondas <artur.kondas@form3.tech>
2022-09-01 10:47:41 +0200

    add test for GetHuman

--- rest/rest_test.go
+++ rest/rest_test.go
@@ -44,9 +44,53 @@ func BenchmarkCreateHumans(b *testing.B) {
 		if err != nil {
 			b.Fatal("Post:", err)
 		}
+		defer res.Body.Close()
 
 		if http.StatusCreated != res.StatusCode {
 			b.Fatal("Unable to create a human!")
+		}
+	}
+}
+
+var gethumans = make(map[int]Human, 0)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 500000; i++ {
+		human := Human{
+			FirstName:  fmt.Sprint(rand.Intn(1000000)),
+			LastName:   fmt.Sprint(rand.Intn(1000000)),
+			Age:        rand.Intn(100),
+			LikesPizza: true,
+		}
+		gethumans[i] = human
+	}
+}
+
+func BenchmarkGetHumans(b *testing.B) {
+	ts := httptest.NewServer(GetHuman(gethumans))
+	defer ts.Close()
+
+	tr := &http.Transport{}
+	defer tr.CloseIdleConnections()
+	cl := &http.Client{
+		Transport: tr,
+	}
+
+	b.ResetTimer()
+
+	for j := 0; j < b.N; j++ {
+		url := fmt.Sprintf("%s?human=%v", ts.URL, j)
+
+		res, err := cl.Get(url)
+		if err != nil {
+			b.Fatal("Get:", err)
+		}
+		defer res.Body.Close()
+
+		if http.StatusOK != res.StatusCode {
+			b.Fatalf("Unable to get a human with id of %v!", j)
 		}
 	}
 }
