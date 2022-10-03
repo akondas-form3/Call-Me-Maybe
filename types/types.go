c0bf8a0a974cf323ff95593c41643278c8c00e3e
Artur Kondas <artur.kondas@form3.tech>
2022-10-03 12:55:34 +0200

    add types' comments

--- types/types.go
+++ types/types.go
@@ -2,9 +2,9 @@ package types
 
 // Human is our type that we'll serialize to/from
 type Human struct {
-	ID         int
-	FirstName  string
-	LastName   string
-	Age        int
-	LikesPizza bool
+	ID         int    // The ID of the human
+	FirstName  string // The name
+	LastName   string // The last name
+	Age        int    // The age
+	LikesPizza bool   // The only question you need to ask a human, ever
 }
